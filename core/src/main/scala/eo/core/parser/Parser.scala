a8b21639c97660d333859a288f9505b4aa5e3e7f
nikololiahim <olomishcak@outlook.com>
2021-07-31 11:28:07 +0300

    correctly parses parameter list (with vararg)

--- core/src/main/scala/eo/core/parser/Parser.scala
+++ core/src/main/scala/eo/core/parser/Parser.scala
@@ -1,5 +1,9 @@
 package eo.core.parser
 
+import eo.core.ast._
+import eo.core.ast.astparams.EOExprOnly
+import higherkindness.droste.data.Fix
+
 import scala.util.parsing.combinator.Parsers
 import scala.util.parsing.input.{NoPosition, Position, Reader}
 
@@ -19,37 +23,42 @@ case class ParserError(msg: String) extends CompilationError
 object Parser extends Parsers {
   override type Elem = Token
 
-  def apply(tokens: Seq[Token]): Either[ParserError, EOProg] = {
+  def apply(tokens: Seq[Token]): Either[ParserError, (Vector[LazyBnd], Option[LazyBnd])] = {
     val reader = new WorkflowTokenReader(tokens)
-    program(reader) match {
+    args(reader) match {
       case Success(result, _) => Right(result)
       case Error(msg, _) => Left(ParserError(msg))
       case Failure(msg, _) => Left(ParserError(msg))
     }
   }
 
-  //  private def identifier: Parser[IDENTIFIER] = {
-  //    accept("identifier", { case id: IDENTIFIER => id })
-  //  }
+  private def identifier: Parser[IDENTIFIER] = {
+    accept("identifier", { case id: IDENTIFIER => id })
+  }
   //
   //  private def literal: Parser[LITERAL] = {
   //    accept("literal", { case lit: LITERAL => lit })
   //  }
 
-  private def single_line_comment: Parser[SINGLE_LINE_COMMENT] = {
-    accept("single line comment", { case comment: SINGLE_LINE_COMMENT => comment })
-  }
+  //  private def single_line_comment: Parser[SINGLE_LINE_COMMENT] = {
+  //    accept("single line comment", { case comment: SINGLE_LINE_COMMENT => comment })
+  //  }
 
   private def meta: Parser[META] = {
     accept("meta", { case meta: META => meta })
   }
 
 
-  def program: Parser[EOProg] = {
-    opt(license) ~ opt(metas) ^^ {
-      case license ~ metas => EOProg(license, metas, Vector())
+  def program: Parser[EOProg[EOExprOnly]] = {
+    opt(metas) ~ objects ^^ {
+      case metas ~ objs =>
+        EOProg(
+          metas.getOrElse(EOMetas(None, Vector())),
+          objs
+        )
     }
   }
+
 
   def metas: Parser[EOMetas] = {
     rep1(meta) ^^ {
@@ -76,102 +85,104 @@ object Parser extends Parsers {
     }
   }
 
-  def license: Parser[EOLicense] = {
-    rep1(single_line_comment) ^^ {
-      lines => EOLicense(lines.map(_.text).mkString)
+  def objects: Parser[Vector[EOBnd[EOExprOnly]]] = {
+    rep(`object`) ^^ {
+      objs => objs.toVector
     }
   }
 
-  //  def objects = {
-  //    rep1(
-  //      rep(single_line_comment) ~ `object`
-  //    )
-  //  }
-  //
-  //  def `object` = {
-  //    anonymous |
-  //      (abstraction | application) |
-  //      opt(tail) ~ rep(
-  //        method ~ opt(htail) ~ opt(suffix) ~ opt(tail)
-  //      )
-  //  }
-  //
-  //  def anonymous = {
-  //    attributes ~ htail
-  //  }
-  //
-  //  def abstraction = {
-  //    rep(single_line_comment) ~ attributes ~ opt(suffix ~ opt(SLASH ~ identifier))
-  //  }
-  //
-  //  def attributes = {
-  //    LBRACKET ~ rep(attribute) ~ RBRACKET
-  //  }
-  //
-  //  def attribute = {
-  //    label
-  //  }
-  //
-  //  def label = {
-  //    PHI | identifier ~ opt(DOTS)
-  //  }
-  //
-  //  def tail = {
-  //    INDENT ~ rep1(`object`) ~ DEDENT
-  //  }
-  //
-  //  def suffix = {
-  //    ASSIGN_NAME ~ label ~ opt(EXCLAMATION_MARK)
-  //  }
-  //
-  //  def method = {
-  //    DOT ~ (identifier | RHO | PHI)
-  //  }
-  //
-  //  def application = {
-  //    htail ~ opt(htail)
-  //  }
-  //
-  //  def htail = {
-  //    rep1(
-  //      head
-  //        | application ~ method
-  //        | LPAREN ~ application ~ RPAREN
-  //        | application ~ has
-  //        | application ~ suffix
-  //        | anonymous
-  //    )
-  //  }
-  //
-  //  def head = {
-  //    PHI | RHO | SELF | ARRAY_DELIMITER | identifier | identifier ~ DOT | data
-  //  }
-  //
-  //  def has = {
-  //    COLON ~ identifier
-  //  }
-  //
-  //  def data = {
-  //    literal ^^ {
-  //      case STRING(value) => EOStrData(value)
-  //      case CHAR(value) => EOCharData(value.charAt(0))
-  //      case INTEGER(value) => EOIntData(value.toInt)
-  //      case FLOAT(value) => EOFloatData(value.toFloat)
-  //    }
-  //  }
+  def `object`: Parser[EOBnd[EOExprOnly]] = {
+    abstraction
+    //    | application
+  }
+
+  def abstraction: Parser[EOBnd[EOExprOnly]] = {
+    anonAbsObj | namedAbsObj
+  }
+
+  def anonAbsObj: Parser[EOAnonExpr[EOExprOnly]] = {
+    args ~ boundAttrs ^^ {
+      case (params, vararg) ~ attrs =>
+        EOAnonExpr(
+          Fix(EOObj(params, vararg, attrs))
+        )
+    }
+  }
+
+  def namedAbsObj: Parser[EOBndExpr[EOExprOnly]] = {
+    args ~ name ~ boundAttrs ^^ {
+      case (params, vararg) ~ name ~ attrs =>
+        EOBndExpr(
+          name,
+          Fix(EOObj(params, vararg, attrs))
+        )
+    }
+  }
+
+  def name: Parser[EONamedBnd] = {
+    ASSIGN_NAME ~> identifier ^^ (id => EOAnyNameBnd(LazyBnd(id.name)))
+  }
+
+  def args: Parser[(Vector[LazyBnd], Option[LazyBnd])] = {
+    val vararg = LBRACKET ~> varargList <~ RBRACKET ^^ { pair =>
+      (pair._1, Some(pair._2))
+    }
+    val noVararg = LBRACKET ~> argList <~ RBRACKET ^^ {
+      vec => (vec, None)
+    }
+    vararg | noVararg
+  }
+
+  def argList: Parser[Vector[LazyBnd]] = {
+    rep(identifier) ^^ {
+      params => params.map(id => LazyBnd(id.name)).toVector
+    }
+  }
+
+  def varargList: Parser[(Vector[LazyBnd], LazyBnd)] = {
+    rep(identifier) <~ DOTS ^^ {
+      ids =>
+        (
+          ids.init.map(id => LazyBnd(id.name)).toVector,
+          LazyBnd(ids.last.name)
+        )
+    }
+  }
+
+  def vararg: Parser[LazyBnd] = {
+    identifier <~ DOTS ^^ (id => LazyBnd(id.name))
+  }
+
+  def boundAttrs: Parser[Vector[EOBndExpr[EOExprOnly]]] = {
+    INDENT ~> rep(namedAbsObj) <~ DEDENT ^^ (attrs => attrs.toVector)
+  }
+
   def main(args: Array[String]): Unit = {
     val code =
       """
-        |# Some license here
-        |# Some more license
         |+package sandbox
-        |+alias  stdout    org.eolang.io.stdout
-        |+alias  sprintf org.eolang.txt.sprintf
-        |+rt  jvm            java8-adopt
-      """.stripMargin
+        |+rt jvm java8
+        |
+        |[] > obj
+        |  [a b] > obj
+        |""".stripMargin
     println(code)
-    println(Lexer(code))
-    println(apply(Lexer(code).getOrElse(List())))
+    val tokens = Lexer("[asd...]") match {
+      case Right(value) => value
+      case Left(value) => throw new Exception(value.msg)
+    }
+
+    println("\nTOKENS:")
+    println(tokens)
+
+    val ast = apply(tokens) match {
+      case Right(value) => value
+      case Left(value) => throw new Exception(value.msg)
+
+    }
+
+    println("\nAST:")
+    println(ast)
   }
 
 }
