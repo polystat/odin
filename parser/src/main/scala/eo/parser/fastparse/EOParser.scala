5dd147bfba6dbc295adcdcdbb72294ac59d1a44c
nikololiahim <olomishcak@outlook.com>
2021-09-05 20:51:55 +0300

    created parser for metas (with a test case)

--- parser/src/main/scala/eo/parser/fastparse/EOParser.scala
+++ parser/src/main/scala/eo/parser/fastparse/EOParser.scala
@@ -1,6 +1,58 @@
 package eo.parser.fastparse
 
-import fastparse._, NoWhitespace._
+import fastparse._
+import NoWhitespace._
+import eo.core.ast.{EOAliasMeta, EOMetas, EORTMeta}
+
+
+object Metas {
+  private def packageMeta[_: P] = P(
+    "+package" ~ " " ~ packageName.! ~ "\n"
+  )
+
+  private def aliasMeta[_:P] = P(
+    "+alias" ~ " " ~ aliasName.! ~ " " ~ packageName.! ~ "\n"
+  ).map {
+    case (alias, src) => EOAliasMeta(alias, src)
+  }
+
+  private def rtMeta[_:P] = P(
+    "+rt" ~ " " ~ aliasName.! ~ " " ~ artifactId.! ~ "\n"
+  ).map {
+    case (rtName, src) => EORTMeta(rtName, src)
+  }
+
+  // TODO: refine rule
+  private def aliasName[_ : P] = P(
+    Tokens.identifier
+  )
+
+  // TODO: refine rule
+  private def artifactName[_:P] =
+    P(Tokens.identifier)
+
+  // TODO: refine rule
+  private def artifactVersion[_:P] =
+    P(Tokens.digit.rep(1).rep(3, sep = "."))
+      .map(_.mkString(sep = "."))
+
+  // TODO: refine rule
+  private def artifactId[_:P] = P(
+    packageName ~/ ":" ~ artifactName ~/ ":" ~ artifactVersion
+  ).map(_.productIterator.mkString(":"))
+
+  // TODO: refine rule
+  private def packageName[_: P] = P(
+    Tokens.identifier.rep(sep = ".", min = 1)
+  ).map(_.mkString(sep="."))
+
+  def metas[_: P]: P[EOMetas] = P(
+    Tokens.emptyLinesOrComments ~ packageMeta.? ~
+      (Tokens.emptyLinesOrComments ~ (rtMeta | aliasMeta))./.rep
+  ).map {
+    case (pkg, metas) => EOMetas(pkg, metas.toVector)
+  }
+}
 
 object Tokens {
 
@@ -17,12 +69,20 @@ object Tokens {
 
   private def uppercase[_: P] = P(CharIn("A-Z"))
 
-  private def digit[_: P] = P(CharIn("0-9"))
+  def digit[_: P]: P[String] = P(CharIn("0-9")).!
 
   def string[_: P]: P[String] = P("\"" ~ CharsWhile(_ != '\"').! ~ "\"")
 
-  def meta[_: P]: P[(String, String)] = P(
-    "+" ~ identifier.! ~ " " ~ CharsWhile(_ != '\n').! ~ "\n"
+  private def whitespaceChars[_: P] = P(
+    CharsWhile(c => c == '\t' || c == ' ' || c == '\r', 0)
+  )
+
+  def emptyLine[_: P]: P[Unit] = P(
+    whitespaceChars ~ "\n"
+  )
+
+  def emptyLinesOrComments[_ : P]: P[Unit] = P(
+    (emptyLine | comment)./.rep
   )
 }
 
--- parser/src/test/scala/eo/parser/fastparse/EOParserTests.scala
+++ parser/src/test/scala/eo/parser/fastparse/EOParserTests.scala
@@ -1,23 +1,56 @@
 package eo.parser.fastparse
+
 import org.scalatest.wordspec.AnyWordSpec
 import eo.parser.fastparse.Tokens._
 import fastparse._
+import NoWhitespace._
+import eo.core.ast.EOMetas
 import org.scalatest.Assertion
 
 
 class EOParserTests extends AnyWordSpec {
+
+  def parseEntireInput[_: P, T](p: => P[T]): P[T] = P(Start ~ p ~ End)
+
+  def metasAllInput[_: P]: P[EOMetas] = parseEntireInput(Metas.metas)
+
 
   def shouldParse[T](parser: P[_] => P[T], input: String): Assertion = {
     val parsed = parse(input, parser)
     println(parsed)
     assert(parsed.isSuccess)
   }
+
+
   "tokens" should {
     "be recognized correctly" in {
+      shouldParse(emptyLine(_), "      \t\n")
       shouldParse(comment(_), "#   oo 121 _= `12 e3\n")
       shouldParse(identifier(_), "a-COOL-identifier")
-      shouldParse(string(_),"\" asd aa dd -= -21 123\"" )
-      shouldParse(meta(_),"+meta some-stuff-H3ЯE   \n" )
+      shouldParse(string(_), "\" asd aa dd -= -21 123\"")
+    }
+  }
+
+  "metas" should {
+    "be recognized correctly" in {
+      shouldParse(metasAllInput(_),
+        """
+          |
+          |+package sandbox
+          |
+          |+rt jvm org.eolang:eo-runtime:0.1.24
+          |+alias stdout org.eolang.io.stdout
+          |
+          |
+          |# some comment here
+          |# some comment
+          |
+          |
+          |
+          |+alias sprintf org.eolang.txt.sprintf
+          |
+          |+alias biba boba
+          |""".stripMargin)
     }
   }
 
