86f3ba08360d788fc8f209329da4060864f5fe9a
nikololiahim <olomishcak@outlook.com>
2021-10-12 16:25:29 +0300

    fixed Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,7 @@ object Dependencies {
     val cats = "2.6.1"
     val catsMtl = "1.2.1"
     val catsEffect = "3.2.8"
+    val catsParse = "0.3.4"
     val scalaTest = "3.2.9"
     val scalaCheck = "3.2.9.0"
     val nonEmpty = "0.2.0"
