853d6f53b8ada3901b456c66c56666d1da8d32cc
nikololiahim <olomishcak@outlook.com>
2021-10-18 16:40:47 +0300

    pretty-printing groups errors by their position

--- parser/src/main/scala/org/polystat/odin/parser/cats_parse/Prettyprint.scala
+++ parser/src/main/scala/org/polystat/odin/parser/cats_parse/Prettyprint.scala
@@ -5,6 +5,7 @@ package org.polystat.odin.parser.cats_parse
 // In particular, from:
 // https://gist.github.com/re-xyr/b0f1988e744c93d4c83a5f17f58484ea
 
+import cats.data.NonEmptyList
 import cats.parse.Parser.Expectation
 import cats.parse._
 import org.polystat.odin.parser.cats_parse.JsonStringUtil.escape
@@ -15,14 +16,14 @@ class Prettyprint(filename: String = "", input: String) {
   def description(x: Parser.Expectation): String = x match {
     case Expectation.OneOfStr(_, strs) =>
       val strList = strs.map { x => s"'${escape('\'', x)}'" }.mkString(", ")
-      s"expected one of $strList"
+      s"one of $strList"
     case Expectation.InRange(_, lower, upper) =>
-      if (lower == upper) s"expected '${escape('\'', lower.toString)}'"
-      else s"expected '$lower' ~ '$upper'"
+      if (lower == upper) s"'${escape('\'', lower.toString)}'"
+      else s"'$lower' ~ '$upper'"
     case Expectation.StartOfString(_) =>
-      "expected beginning of file"
+      "beginning of file"
     case Expectation.EndOfString(_, _) =>
-      "expected end of file"
+      "end of file"
     case Expectation.Length(_, expected, actual) =>
       s"unexpected eof; expected ${expected - actual} more characters"
     case Expectation.ExpectedFailureAt(_, matched) =>
@@ -32,22 +33,45 @@ class Prettyprint(filename: String = "", input: String) {
     case Expectation.FailWith(_, message) =>
       message
     case Expectation.WithContext(contextStr, _) =>
-      s"expected ${escape('\'', contextStr)}"
+      s"context: ${escape('\'', contextStr)}"
   }
 
-  def prettyprint(x: Parser.Expectation): String = {
-    val (row, col) = locmap.toLineCol(x.offset).getOrElse((0, 0))
+  def prettyprint(offset: Int, x: NonEmptyList[Parser.Expectation]): String = {
+    val (row, col) = locmap.toLineCol(offset).getOrElse((0, 0))
     val (r, c) = (row + 1, col + 1)
     val line: String = locmap.getLine(row).get
     val offending =
       s"${row.toString map { _ => ' ' }} | ${" " * col}^"
     s"""
-       |$filename:$r:$c: error: ${description(x)}
-       |$r | $line
+       |$filename:$r:$c: error: expected one of:
+       |    ${x.map(err => description(err)).toList.mkString("\n    ")}
+       |$r| $line
        |$offending""".stripMargin
   }
 
   def prettyprint(x: Parser.Error): String =
-    x.expected.map(prettyprint).toList.mkString("")
+    x.expected
+      .groupBy(_.offset)
+      .map { case (offset, errors) =>
+        prettyprint(offset, errors)
+      }
+      .mkString(", ")
+
+}
+
+object Prettyprint {
+
+  import org.polystat.odin.parser.cats_parse.Parser.`object`
+
+  def main(args: Array[String]): Unit = {
+    val code = "1"
+    val parsed = `object`(0, 2).parseAll(code)
+    println(
+      parsed match {
+        case Left(value) => new Prettyprint(input = code).prettyprint(value)
+        case Right(value) => value
+      }
+    )
+  }
 
 }
