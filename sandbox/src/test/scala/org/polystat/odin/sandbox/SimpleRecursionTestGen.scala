6dc1f684de74eb9fe19067f82f38cd056a1f9194
nikololiahim <olomishcak@outlook.com>
2021-11-17 19:04:02 +0300

    generator is guaranteed to generate 2 mutually recursive objects

--- sandbox/src/test/scala/org/polystat/odin/sandbox/SimpleRecursionTestGen.scala
+++ sandbox/src/test/scala/org/polystat/odin/sandbox/SimpleRecursionTestGen.scala
@@ -1,6 +1,10 @@
 package org.polystat.odin.sandbox
 
+import cats.Eq
+import cats.effect.IO
+import cats.syntax.eq._
 import org.scalacheck.{Gen, Prop, Test}
+import fs2.Stream
 
 case class MethodCall(name: String) {
 
@@ -12,7 +16,6 @@ case class MethodCall(name: String) {
 case class Method(name: String, calls: List[MethodCall]) {
 
   override def toString: String = {
-
     val methodBody = calls match {
       case Nil => ""
       case head :: Nil => s"""  $head > @""".stripMargin
@@ -20,10 +23,8 @@ case class Method(name: String, calls: List[MethodCall]) {
         s"""  seq > @
            |      ${lst.mkString("\n      ")}""".stripMargin
     }
-
     s"""[self] > $name
        |  $methodBody""".stripMargin
-
   }
 
 }
@@ -57,40 +58,58 @@ object SimpleRecursionTestGen {
     )
   )
 
-  def method(name: String): Gen[Method] = for {
-    calls <- Gen
-      .atLeastOne(methods.filter(_ != name)) // method shouldn't call itself
-  } yield Method(name, calls.map(MethodCall).toList)
-
-  def topLevelObj(name: String): Gen[TopLevelObj] = for {
-    names <- Gen.atLeastOne(methods)
-    methods <- Gen.sequence[List[Method], Method](names.map(method)) // methods should have different names
-  } yield TopLevelObj(name, methods)
-
-  val objs: Gen[List[TopLevelObj]] = for {
-    names <- Gen.atLeastOne(toplevelObjs)
-    objs <- Gen.sequence[List[TopLevelObj], TopLevelObj](names.map(topLevelObj))
-  } yield objs
-
   def containsUnique[T](s: Seq[T]): Boolean = {
     Set.from(s).size == s.length
   }
 
+  def between[T](min: Int, max: Int, seq: Seq[T]): Gen[Seq[T]] = for {
+    size <- Gen.choose(min, max)
+    seq <- Gen.pick(size, seq)
+  } yield seq.toSeq
+
+  def method(methodPool: List[String])(name: String): Gen[Method] = for {
+    call <- Gen.oneOf(methodPool.filter(_ != name)) // method shouldn't call itself
+  } yield Method(name, (call :: Nil).map(MethodCall))
+
+  def topLevelObj(
+    methodPool: List[String]
+  )(methodCallPool: List[String])(name: String): Gen[TopLevelObj] =
+    for {
+      names <- Gen.const(methodPool)
+      methods <-
+        Gen.sequence[List[Method], Method](names.map(method(methodCallPool))) // methods should have different names
+    } yield TopLevelObj(name, methods)
+
+  def objs(methodDeclPool: List[String]): Gen[List[TopLevelObj]] = for {
+    aMethods <- Gen.pick(2, methodDeclPool).map(_.toList)
+    bMethods = methodDeclPool.filter(name => !aMethods.contains(name))
+    a <- topLevelObj(aMethods)(bMethods)("a")
+    b <- topLevelObj(bMethods)(aMethods)("b")
+  } yield List(a, b)
+
   def main(args: Array[String]): Unit = {
-    Prop
-      .forAll[List[TopLevelObj], Boolean](objs)(objs => {
-        val objNames: List[String] = objs.map(_.name)
-        val objNamesUnique: Boolean = containsUnique(objNames)
-        val methodNamesUnique: List[Boolean] = for {
-          obj <- objs
-          methodNames = obj.defines.map(_.name)
-        } yield containsUnique(methodNames)
 
-        (objNamesUnique :: methodNamesUnique).forall(p => p)
-      })
-      .check(Test.Parameters.default.withMinSuccessfulTests(1e5.toInt))
+    Gen
+      .listOfN(1, objs(methods))
+      .sample
+      .foreach(_.foreach(s => println(s.mkString("\n"))))
 
-    objs.sample.map(_.mkString("\n")).foreach(println)
+    import cats.effect.unsafe.implicits.global
+    val max = 1e7.toInt
+    Stream
+      .range(0, max)
+      .evalMap[IO, List[TopLevelObj]](i =>
+        IO.print(f"${(i / max.toDouble) * 100}%1.2f%%\r") >>
+          IO.pure(objs(methods).sample.get)
+      )
+      .dropWhile(_ != exampleAst)
+      .compile
+      .toList
+      .map(_.headOption)
+      .flatMap(sample =>
+        IO.delay(sample.foreach(l => println(l.mkString("\n"))))
+      )
+      .unsafeRunSync()
   }
 
 }
