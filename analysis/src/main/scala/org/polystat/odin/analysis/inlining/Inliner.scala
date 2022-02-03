10d137deacb5348ef8f6623fad71890eabbcfcbc
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-02-03 15:53:30 +0300

    Fix(Analysis. inlining.Inliner): Proper handling of references to self

--- analysis/src/main/scala/org/polystat/odin/analysis/inlining/Inliner.scala
+++ analysis/src/main/scala/org/polystat/odin/analysis/inlining/Inliner.scala
@@ -2,7 +2,7 @@ package org.polystat.odin.analysis.inlining
 
 import higherkindness.droste.data.Fix
 import org.polystat.odin.analysis.inlining.Context.setLocators
-import org.polystat.odin.core.ast._
+import org.polystat.odin.core.ast.{EODecoration, _}
 import org.polystat.odin.parser.eo.Parser
 import org.polystat.odin.backend.eolang.ToEO.instances.progToEO
 import org.polystat.odin.backend.eolang.ToEO.ops.ToEOOps
@@ -15,34 +15,50 @@ object Big {
 object Inliner {
   case class Method(name: String, body: EOObj[Fix[EOExpr]])
 
-  def extractMethods(obj: EOObj[Fix[EOExpr]]): List[Method] = {
-    obj
+  case class MethodList(methods: List[Method], depth: BigInt)
+
+  def extractMethods(
+    obj: EOObj[Fix[EOExpr]],
+    depth: BigInt
+  ): MethodList = {
+    val methods = obj
       .bndAttrs
       .flatMap {
         case EOBndExpr(EOAnyNameBnd(LazyName(methodName)), Fix(expr)) =>
           expr match {
-            case method @ EOObj(LazyName("self") +: _, _, _) =>
-              Some(Method(methodName, method))
+            case method @ EOObj(LazyName("self") +: _, _, bndAttrs) =>
+              bndAttrs
+                .find {
+                  case EOBndExpr(EODecoration, _) => true
+                  case _ => false
+                }
+                .map(_ => Method(methodName, method))
             case _ => None
           }
         case _ => None
       }
       .toList
+
+    MethodList(methods, depth)
   }
 
   def inlineCall(
     call: EOCopy[Fix[EOExpr]],
-    availableMethods: List[Method]
+    availableMethods: MethodList
   ): Either[String, EOExpr[Fix[EOExpr]]] = {
-    def helper(value: EOCopy[Fix[EOExpr]]): EOExpr[Fix[EOExpr]] = ???
+    def inline(
+      call: EOCopy[Fix[EOExpr]],
+      method: Method
+    ): EOExpr[Fix[EOExpr]] = ???
 
     call.trg match {
-      case EODot(EOSimpleAppWithLocator("self", Big(0)), methodName) =>
+      case EODot(_, methodName) =>
         availableMethods
+          .methods
           .find(method => method.name == methodName)
           .map(method =>
             if (call.args.length == method.body.freeAttrs.length)
-              Right(helper(call))
+              Right(inline(call, method))
             else
               Left(s"Wrong number of arguments given for method $methodName.")
           )
@@ -57,57 +73,66 @@ object Inliner {
   ): Either[String, EOProg[Fix[EOExpr]]] = {
 
     def exprHelper(
-      availableMethods: List[Method]
+      availableMethods: MethodList,
+      currentDepth: BigInt
     )(
       expr: EOExpr[Fix[EOExpr]]
     ): Either[String, EOExpr[Fix[EOExpr]]] = expr match {
-      // Method
       case method @ EOObj(LazyName("self") +: _, _, bndAttrs) =>
         bndAttrs
-          .traverse(bndExprHelper(availableMethods))
+          .traverse(bndExprHelper(availableMethods, currentDepth + 1))
           .map(bnds => method.copy(bndAttrs = bnds))
 
-      // Object
       case obj @ EOObj(_, _, bndAttrs) =>
+        val newDepth = currentDepth + 1
+        val newMethods = extractMethods(obj, newDepth)
         bndAttrs
-          .traverse(bndExprHelper(extractMethods(obj)))
+          .traverse(bndExprHelper(newMethods, newDepth))
           .map(bnds => obj.copy(bndAttrs = bnds))
 
       case dot @ EODot(Fix(src), _) =>
-        exprHelper(availableMethods)(src).map(src => dot.copy(src = Fix(src)))
+        exprHelper(availableMethods, currentDepth)(src).map(src =>
+          dot.copy(src = Fix(src))
+        )
 
-      case call: EOCopy[Fix[EOExpr]] => inlineCall(call, availableMethods)
+      case call @ EOCopy(
+             EODot(EOSimpleAppWithLocator("self", locator), _),
+             _
+           ) if availableMethods.depth == currentDepth - locator =>
+        inlineCall(call, availableMethods)
 
       case other => Right(other)
     }
 
     def bndExprHelper(
-      availableMethods: List[Method]
+      availableMethods: MethodList,
+      currentDepth: BigInt
     )(
       bnd: EOBndExpr[Fix[EOExpr]]
     ): Either[String, EOBndExpr[Fix[EOExpr]]] =
-      exprHelper(availableMethods)(Fix.un(bnd.expr)).map(expr =>
+      exprHelper(availableMethods, currentDepth)(Fix.un(bnd.expr)).map(expr =>
         bnd.copy(expr = Fix(expr))
       )
 
     def bndHelper(
-      availableMethods: List[Method]
+      availableMethods: MethodList,
+      currentDepth: BigInt
     )(
       bnd: EOBnd[Fix[EOExpr]]
     ): Either[String, EOBnd[Fix[EOExpr]]] =
       bnd match {
         case EOAnonExpr(Fix(expr)) =>
-          exprHelper(availableMethods)(expr).map(value =>
+          exprHelper(availableMethods, currentDepth)(expr).map(value =>
             EOAnonExpr(Fix(value))
           )
 
         case bnd: EOBndExpr[Fix[EOExpr]] =>
-          bndExprHelper(availableMethods)(bnd)
+          bndExprHelper(availableMethods, currentDepth)(bnd)
       }
 
     prog
       .bnds
-      .traverse(bndHelper(List.empty))
+      .traverse(bndHelper(MethodList(List.empty, 0), 0))
       .map(bnds => prog.copy(bnds = bnds))
   }
 
@@ -116,15 +141,16 @@ object Inliner {
       """
         |
         |[] > outer
-        |  [] > self
-        |    256 > magic
-        |    [] > dummy
-        |      [self] > aboba
-        |        22 > @
-        |      [self outer] > innerMethod
-        |        self.aboba self > @
-        |      self.innerMethod self self > @
-        |    self "yahoo" > @
+        |  256 > magic
+        |  [] > dummy
+        |    [self] > bMethod
+        |      22 > @
+        |    [self outer] > innerMethod
+        |      [self] > innerInnerMethod
+        |        ^.^.self.bMethod ^.self > @
+        |      ^.self.bMethod ^.self > @
+        |    self.innerMethod self self > @
+        |  self "yahoo" > @
         |  [self] > method
         |    self.magic > @
         |     
