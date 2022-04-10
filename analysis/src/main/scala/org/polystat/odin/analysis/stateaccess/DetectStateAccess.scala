ac59137242d6e3eb5d4ec677165ea25e22af72f1
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-04-11 00:02:06 +0300

    feat(analysis.stateAccess): support for more obscure defect locations

--- analysis/src/main/scala/org/polystat/odin/analysis/stateaccess/DetectStateAccess.scala
+++ analysis/src/main/scala/org/polystat/odin/analysis/stateaccess/DetectStateAccess.scala
@@ -2,24 +2,62 @@ package org.polystat.odin.analysis.stateaccess
 
 import cats.data.EitherNel
 import cats.syntax.either._
+import higherkindness.droste.data.Fix
 import org.polystat.odin.analysis.inlining._
 import org.polystat.odin.core.ast._
+import org.polystat.odin.core.ast.astparams.EOExprOnly
 import org.polystat.odin.parser.eo.Parser
+
+import scala.annotation.tailrec
 
 object DetectStateAccess {
 
   type ObjInfo = ObjectInfo[ParentInfo[MethodInfo, ObjectInfo], MethodInfo]
-  case class State(container: EONamedBnd, states: Vector[EONamedBnd])
-  case class StateChange(method: EONamedBnd, state: EONamedBnd)
+
+  case class State(
+    containerName: String,
+    statePath: List[String],
+    states: Vector[EONamedBnd]
+  )
+
+  case class StateChange(
+    method: EONamedBnd,
+    state: EONamedBnd,
+    statePath: List[String]
+  )
+
+  def collectNestedStates(mainParent: String)(
+    subTree: Inliner.CompleteObjectTree
+  ): Vector[State] = {
+    val currentLvlStateNames = subTree
+      .info
+      .bnds
+      .collect {
+        case BndItself(
+               EOBndExpr(
+                 bndName,
+                 EOSimpleAppWithLocator("memory" | "cage", _)
+               )
+             ) => bndName
+      }
+
+    Vector(
+      State(mainParent, subTree.info.fqn.names.tail, currentLvlStateNames)
+    ) ++
+      subTree
+        .children
+        .flatMap(t => collectNestedStates(mainParent)(t._2))
+  }
 
   def accumulateParentState(tree: Map[EONamedBnd, Inliner.CompleteObjectTree])(
     currentParentLink: Option[ParentInfo[MethodInfo, ObjectInfo]],
-    existingState: Vector[EONamedBnd] = Vector()
-  ): List[State] = {
+    existingStates: Vector[EONamedBnd] = Vector()
+  ): Vector[State] = {
     currentParentLink match {
       case Some(parentLink) =>
         val parentObj = parentLink.linkToParent.getOption(tree).get
-        val currentStates = parentObj
+        val currentObjName = parentObj.info.name.name.name
+        val currentLvlStateNames = parentObj
           .info
           .bnds
           .collect {
@@ -28,26 +66,65 @@ object DetectStateAccess {
                      bndName,
                      EOSimpleAppWithLocator("memory" | "cage", _)
                    )
-                 ) if !existingState.contains(bndName) =>
+                 ) if !existingStates.contains(bndName) =>
               bndName
           }
+        val currentLvlState =
+          State(currentObjName, List(), currentLvlStateNames)
+        val nestedStates = parentObj
+          .children
+          .flatMap(c =>
+            collectNestedStates(parentObj.info.name.name.name)(c._2)
+          )
+          .toVector
 
-        List(State(container = parentObj.info.name, states = currentStates)) ++
+        Vector(currentLvlState) ++ nestedStates ++
           accumulateParentState(tree)(
             parentObj.info.parentInfo,
-            existingState ++ currentStates
+            existingStates ++ currentLvlStateNames
           )
 
-      case None => List()
+      case None => Vector()
     }
   }
 
   def getAccessedStates(method: (EONamedBnd, MethodInfo)): List[StateChange] = {
+    @tailrec
+    def hasSelfAsSource(dot: EODot[EOExprOnly]): Boolean = {
+      Fix.un(dot.src) match {
+        case EOSimpleAppWithLocator("self", x) if x == 0 => true
+        case innerDot @ EODot(_, _) => hasSelfAsSource(innerDot)
+        case _ => false
+      }
+    }
+
+    def buildDotChain(dot: EODot[EOExprOnly]): List[String] =
+      Fix.un(dot.src) match {
+        case EOSimpleAppWithLocator("self", x) if x == 0 => List()
+        case innerDot @ EODot(_, _) =>
+          buildDotChain(innerDot).appended(innerDot.name)
+        case _ => List()
+      }
+
     val binds = method._2.body.bndAttrs
 
+    def processDot(
+      innerDot: EODot[Fix[EOExpr]],
+      state: String
+    ): List[StateChange] = {
+      val stateName = EOAnyNameBnd(LazyName(state))
+      val containerChain = buildDotChain(innerDot)
+
+      List(StateChange(method._1, stateName, containerChain))
+    }
+
     Abstract.foldAst[List[StateChange]](binds) {
-      case EODot(EOSimpleAppWithLocator("self", x), state) if x == 0 =>
-        List(StateChange(method._1, EOAnyNameBnd(LazyName(state))))
+      case EOCopy(Fix(dot @ EODot(Fix(innerDot @ EODot(_, state)), _)), _)
+           if hasSelfAsSource(dot) =>
+        processDot(innerDot, state)
+
+      case dot @ EODot(_, state) if hasSelfAsSource(dot) =>
+        processDot(dot, state)
     }
   }
 
@@ -59,17 +136,17 @@ object DetectStateAccess {
     val accessedStates = obj._2.info.methods.flatMap(getAccessedStates)
     val results =
       for {
-        StateChange(changeSource, state) <- accessedStates
-        State(stateContainer, changedStates) <- availableParentStates
+        StateChange(targetMethod, state, accessedStatePath) <- accessedStates
+        State(baseClass, statePath, changedStates) <- availableParentStates
       } yield
-        if (changedStates.contains(state)) {
+        if (changedStates.contains(state) && statePath == accessedStatePath) {
           val objName = obj._2.info.fqn.names.toList.mkString(".")
           val stateName = state.name.name
-          val method = changeSource.name.name
-          val container = stateContainer.name.name
+          val method = targetMethod.name.name
+          val container = statePath.prepended(baseClass).mkString(".")
 
           List(
-            f"Method '${method}' of object '${objName}' directly accesses state '${stateName}' of base class '${container}'"
+            f"Method '$method' of object '$objName' directly accesses state '$stateName' of base class '$container'"
           )
         } else List()
 
@@ -83,7 +160,6 @@ object DetectStateAccess {
       tree: Map[EONamedBnd, Inliner.CompleteObjectTree]
     ): List[String] =
       tree
-        // Has a parent
         .filter(_._2.info.parentInfo.nonEmpty)
         .flatMap(detectStateAccesses(originalTree))
         .toList
