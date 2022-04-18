a1db50fc4cebd9e6f3d8b751c3cb1b2ac2c7dc7c
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-04-18 03:30:13 +0300

    test(analysis.extractLogic): added an additional test case without the defect

--- analysis/src/test/scala/org/polystat/odin/analysis/UnjustifiedAssumptionTests.scala
+++ analysis/src/test/scala/org/polystat/odin/analysis/UnjustifiedAssumptionTests.scala
@@ -5,17 +5,22 @@ import org.scalatest.wordspec.AnyWordSpec
 import EOOdinAnalyzer.unjustifiedAssumptionAnalyzer
 import org.polystat.odin.parser.EoParser.sourceCodeEoParser
 import cats.effect.unsafe.implicits.global
+import org.polystat.odin.analysis.EOOdinAnalyzer.OdinAnalysisResult._
 
 class UnjustifiedAssumptionTests extends AnyWordSpec {
 
   case class TestCase(label: String, code: String, expected: List[String])
 
-  def analyze(code: String): IO[List[String]] =
-    EOOdinAnalyzer
-      .analyzeSourceCode[String, IO](unjustifiedAssumptionAnalyzer[IO])(code)(sourceCodeEoParser())
-      .compile
-      .toList
-      .map(_.map(_.value))
+  def analyze(code: String): IO[List[String]] =EOOdinAnalyzer
+    .analyzeSourceCode[String, IO](unjustifiedAssumptionAnalyzer)(code)(
+      cats.Monad[IO],
+      sourceCodeEoParser()
+    )
+    .flatMap {
+      case Ok(_) => IO.pure(List.empty)
+      case DefectDetected(_, message) => IO.pure(List(message))
+      case AnalyzerFailure(_, e) => IO.raiseError(e)
+    }
 
   val testCasesWithErrors = List(
     TestCase(
@@ -198,6 +203,22 @@ class UnjustifiedAssumptionTests extends AnyWordSpec {
   val testCasesWithoutErrors = List(
     TestCase(
       label = "All methods are referentially transparent",
+      code =
+        """[] > test
+          |  [] > base
+          |    [self v] > n
+          |      2 > @
+          |    [self v] > m
+          |      self.n self v > @
+          |  [] > derived
+          |    base > @
+          |    [self v] > n
+          |      33 > @
+          |""".stripMargin,
+      expected = List()
+    ),
+    TestCase(
+      label = "All methods are referentially transparent with recursion",
       code =
         """[] > test
           |  [] > base
