ee6c805d076e4876ad2681da4872c9be0366dfb0
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-06-13 10:40:58 +0300

    Feat(analysis.multpleFiles): sharing of top-level objects within a package

--- analysis/src/main/scala/org/polystat/odin/analysis/utils/ImportProcessing.scala
+++ analysis/src/main/scala/org/polystat/odin/analysis/utils/ImportProcessing.scala
@@ -10,24 +10,65 @@ import org.polystat.odin.core.ast.astparams.EOExprOnly
 object ImportProcessing {
 
   case class Importable(
-    src: NonEmptyList[String],
-    content: EOExprOnly
-  )
+    pkg: Option[String],
+    path: NonEmptyList[String],
+    content: EOExprOnly,
+    srcFile: String
+  ) {
+    val fullSrc: NonEmptyList[String] = pkg.map(path.prepend).getOrElse(path)
+    val isTopLvl: Boolean = path.length == 1
+
+    def toBndExpr(alias: Option[String] = None): EOBndExpr[EOExprOnly] = {
+      val name = alias.getOrElse(path.last)
+      EOBndExpr(EOAnyNameBnd(LazyName(name)), content)
+    }
+
+  }
 
   def fetchAvailableImportables(src: List[String])(
     bnd: EOBndExpr[EOExprOnly]
-  )(implicit pkg: Option[String]): List[Importable] = {
+  )(implicit pkg: Option[String], sourceFile: String): List[Importable] = {
     val newPath =
       NonEmptyList.ofInitLast(src, bnd.bndName.name.name)
-    val fullPath = pkg.map(newPath.prepend).getOrElse(newPath)
 
     bnd.expr match {
       case obj @ Fix(EOObj(_, _, bndAttrs)) =>
         List(
-          Importable(fullPath, obj)
+          Importable(pkg, newPath, obj, sourceFile)
         ) ++ bndAttrs.flatMap(fetchAvailableImportables(newPath.toList))
-      case other => List(Importable(fullPath, other))
+      case other => List(Importable(pkg, newPath, other, sourceFile))
     }
+  }
+
+  def fetchImports(
+    availableImports: List[Importable]
+  )(meta: EOMetas): Option[Vector[EOBndExpr[EOExprOnly]]] = {
+    val currentPkg = meta.pack
+
+    val pkgImports = availableImports.collect {
+      case imp @ Importable(pkg, _, _, _)
+           if pkg == currentPkg && imp.isTopLvl => imp.toBndExpr()
+    }
+
+    val nonPkgImports =
+      meta
+        .metas
+        .filter {
+          // Todo: check a special case where the pkg name eq the import name
+          case EOAliasMeta(_, src) if !currentPkg.contains(src.head) => true
+          case _ => false
+        }
+        .traverse {
+          case EOAliasMeta(alias, src) =>
+            for {
+              imp <-
+                availableImports
+                  .find(imp => imp.fullSrc == src)
+            } yield imp.toBndExpr(alias)
+          case _ => None
+        }
+
+    nonPkgImports.map(_ ++ pkgImports)
   }
 
   def prependImports[F[_]](
@@ -36,33 +77,19 @@ object ImportProcessing {
     ae: ApplicativeError[F, Throwable]
   ): F[Map[String, EOProg[EOExprOnly]]] = {
     val availableImports = for {
-      prog <- fileToAst.values
+      (file, prog) <- fileToAst.toList
       pkg = prog.metas.pack
       importable <- prog
         .bnds
-        .collect { case bnd @ EOBndExpr(_, _) => bnd } flatMap (bnd =>
-        fetchAvailableImportables(List())(bnd)(pkg)
-      )
+        .collect { case bnd @ EOBndExpr(_, _) => bnd }
+        .flatMap(bnd => fetchAvailableImportables(List())(bnd)(pkg, file))
     } yield importable
-
-    def attemptFetchImport: EOMeta => Option[EOBndExpr[EOExprOnly]] = {
-      case EOAliasMeta(alias, src) =>
-        for {
-          imp <-
-            availableImports
-              .find(imp => imp.src == src)
-          boundObj = imp.content
-          name = alias.getOrElse(src.last)
-        } yield EOBndExpr(EOAnyNameBnd(LazyName(name)), boundObj)
-      case _ => None
-    }
 
     ae.fromOption(
       fileToAst
         .toList
-        .traverse { case (fileName, prog @ EOProg(EOMetas(_, metas), _)) =>
-          metas
-            .traverse(attemptFetchImport)
+        .traverse { case (fileName, prog @ EOProg(meta, _)) =>
+          fetchImports(availableImports)(meta)
             .map(importBnds =>
               (fileName, prog.copy(bnds = prog.bnds.prependedAll(importBnds)))
             )
